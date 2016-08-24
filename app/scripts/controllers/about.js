1ee229b85d639f7b10f9ed89e4ab040a6d4ff611
Charlotte Fereday <cbrfereday@gmail.com>
2016-08-24 18:48:01 +0100

    created angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hackbrexitgithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hackbrexitgithubioApp
+ */
+angular.module('hackbrexitgithubioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hackbrexitgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hackbrexitgithubioApp
+ */
+angular.module('hackbrexitgithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
